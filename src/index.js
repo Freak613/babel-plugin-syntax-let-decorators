e9ae1a5064c6b90b8d9964b2c06faeadf98b0d06
ukari <muromi.ukari@gmail.com>
2017-12-27 04:25:02 +0900

    update for lint

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ export default function() {
   return {
     inherits: require("@babel/plugin-syntax-decorators").default,
     manipulateOptions(opts, parserOpts) {
-      parserOpts.allowLetDecorators = 'true';
+      parserOpts.allowLetDecorators = "true";
     },
   };
 }
